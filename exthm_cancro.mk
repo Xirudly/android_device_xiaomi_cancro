b0a84059d2593958e7c607d44b55d12951cea4f6
Xirudly <734428735@qq.com>
2021-10-01 12:01:15 +0000

    cancro: exTHmUI Bringup Signed-off-by: Xirudly <734428735@qq.com>

--- exthm_cancro.mk
+++ exthm_cancro.mk
@@ -16,13 +16,13 @@
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common exTHmUI stuff.
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 # Shipping API level
-$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
+$(call inherit-product, vendor/exthm/build/target/product/product_launched_with_j_mr2.mk)
 
-PRODUCT_NAME := lineage_cancro
+PRODUCT_NAME := exthm_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
